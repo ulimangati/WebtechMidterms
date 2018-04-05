5fb1ac8ed2f9cb7e9b69f42dd2e8410081a9d7ad
Von Manaois <35596510+vondcm@users.noreply.github.com>
2018-04-06 03:54:53 +0800

    Add files via upload
    Html and css of pwa

---
+++ Progressive Web App/bsg.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("Score1");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("SCORE:",10,50);
+
+var canvas = document.getElementById("Score2");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("SCORE:",10,50);
