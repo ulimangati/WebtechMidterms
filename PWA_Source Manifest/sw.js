648a46a71496af7bbfa2fe4b6cd81a08d3a19940
Ojoj <2143697@slu.edu.ph>
2018-04-16 08:43:31 +0800

    Web App with Manifest

---
+++ PWA_Source Manifest/sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/js/app.js',
+          '/css/app.css',
+          '/images/logo.jpg',
+          'https://fonts.googleapis.com/css?family=Raleway:400,700'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
